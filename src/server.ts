74aaecc7e171df2e5a5af9087d0824c599c0a19b
hoangman <hoangman772@gmail.com>
2020-10-13 22:59:49 +0700

    update

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import http from 'http';
 import router from './routers/index';
 import logger from './Log';
-
 const app = express();
 
 app.use('/', router);
 
-app.listen(3000, () => {
+
+
+var server = http.createServer(app);
+server.listen(3000, () => {
   logger.info('Listening on port 3000');
 });
