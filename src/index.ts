7b3b0bf4a5b4a2c3e21ae2ab0ad52b80049f307b
hoang man <hoangman772@gmail.com>
2020-10-14 09:33:22 +0700

    fix

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,20 @@ import logger from './Log';
 const app = express();
 
 app.use('/', router);
-
+var port = process.env.PORT || 3000;
+app.set('port', port);
 
 
 var server = http.createServer(app);
-server.listen(3000, () => {
-  logger.info('Listening on port 3000');
-});
+server.listen(3000);
+server.on('listening', onListening);
+
+
+function onListening() {
+  var addr:any = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+    console.log(addr);
+    console.log(bind);
+}
